04754b5293d35343ec67b1656ae58b6d6f7e1c86
Wojciech Siekiera <wojciech@siekiera.net>
2019-11-19 11:31:57 +0100

    Test commit.

--- src/main/java/net/siekiera/hellodocker/MainController.java
+++ src/main/java/net/siekiera/hellodocker/MainController.java
@@ -1,12 +1,17 @@
 package net.siekiera.hellodocker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+    Logger logger = LoggerFactory.getLogger(MainController.class);
+
     @GetMapping("/")
     public String hello() {
-        return "Hello World from docker.";
+        logger.info("hello method called!");
+        return "Hello World from docker!";
     }
 }
